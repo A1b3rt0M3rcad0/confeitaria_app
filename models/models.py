8b3c282633c917739f47902e0923b7e5b598c428
alberto <alberto.zimba2013@gmail.com>
2024-07-16 16:49:41 -0300

    first_commit

---
+++ models/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+
+class Base(DeclarativeBase):
+    pass
