9cdbd01997870b66ecad36a6873c9f38faf6f522
alberto <alberto.zimba2013@gmail.com>
2024-07-20 06:19:43 -0300

    add_logger.py

--- controllers/base_controller.py
+++ controllers/base_controller.py
@@ -1,31 +1,12 @@
-import logging
-from config.settings import Config
 from models.models import Base
+from config.logger import init_logger
 from typing import List
 from sqlalchemy import Engine
 from sqlalchemy.orm import Session
 from sqlalchemy import select as sqlalchemy_select
 from sqlalchemy.exc import IntegrityError, ArgumentError, ProgrammingError
 
-# logger
-logger = logging.getLogger(**Config.logger)
-logger.setLevel(logging.DEBUG)  # Definindo o nível de log
-
-# Criando um manipulador para escrever logs em um arquivo
-file_handler = logging.FileHandler(filename=Config.logger['name'])
-
-# Criando um manipulador para exibir logs no console
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)  # Todos os logs serão exibidos no console
-
-# Criando um formatador e adicionando-o aos manipuladores
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-console_handler.setFormatter(formatter)
-
-# Adicionando os manipuladores ao logger
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
+logger = init_logger()
 
 class BaseController:
 
